0db5ab2e04a4a4907188bf4b8a3322bd2d2ee7d0
aysha3f <tahiaysha220@gmail.com>
2025-10-04 06:09:27 +0600

    summary

--- src/summary.jsx
+++ src/summary.jsx
@@ -1,4 +1,6 @@
 /**
- * 
- * 
+ * component --> a small unit a website: just JS function
+ * jsx --> html like js
+ * conditional rendering --> 6
+ * how to loop through and array and show component
  */
